c5bec9ed5e61532d07f4463c2c1e1e0a58b5e34b
azder <azhder@gmail.com>
2017-03-13 22:08:11 +0100

    first commit

---
+++ src/string.php
@@ -0,0 +1,73 @@
+<?php
+/** Created by User: azder on Date: 13/03/2017 */
+
+namespace Azder\S;
+
+const string_empty = '';
+const string_space = ' ';
+const string_aster = '*';
+const string_ellip = '...';
+
+
+function concat( ...$args ): string
+{
+    return implode( string_empty, $args );
+}
+
+
+function glue( string $glue, ...$args ): string
+{
+    return implode( $glue, $args );
+}
+
+function gluer( string $glue ): callable
+{
+    return function ( ...$args ) use ( $glue ) : string
+    {
+        return implode( $glue, $args );
+    };
+}
+
+
+function wrap( string $wrap, $value ): string
+{
+    return string_empty . $wrap . $value . $wrap;
+}
+
+function wrapper( string $wrap ): callable
+{
+    return function ( $value ) use ( $wrap ) : string
+    {
+        return string_empty . $wrap . $value . $wrap;
+    };
+}
+
+
+function suffix( string $suffix, $value ): string
+{
+    return string_empty . $value . $suffix;
+}
+
+function suffixer( string $suffix ): callable
+{
+    return function ( $value ) use ( $suffix ) : string
+    {
+        return string_empty . $value . $suffix;
+    };
+}
+
+
+function prefix( string $prefix, $value ): string
+{
+    return string_empty . $prefix . $value;
+}
+
+function prefixer( string $prefix ): callable
+{
+    return function ( $value ) use ( $prefix ) : string
+    {
+        return string_empty . $prefix . $value;
+    };
+}
+
+
